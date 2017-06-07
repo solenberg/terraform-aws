40f9172a469b54a89b9e11886b584089e4ab3006
Solenberg <git@solenberg.net>
2017-06-06 20:30:47 -0400

    nat_instnce.tf merged into vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,6 +10,90 @@ resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.default.id}"
 }
 
+/*==============
+  Nat Instance
+===============*/
+
+resource "aws_security_group" "nat" {
+  name = "vpc_nat"
+  description = "Allow traffic to pass from private sub to           internet"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+  egress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags {
+    Name = "natSG"
+  }
+}
+
+resource "aws_instance" "nat" {
+  ami = "ami-07fdd962"
+  availability_zone = "us-east-2a"
+  instance_type = "t2.micro"
+  key_name = "${var.aws_key_name}"
+  vpc_security_group_ids = ["${aws_security_group.nat.id}"]
+  subnet_id = "${aws_subnet.us-east-2a-public.id}"
+  associate_public_ip_address = true
+  source_dest_check = false
+
+  tags {
+    Name = "VPC nat"
+  }
+}
+
+resource = "aws_eip" "nat" {
+  instance = "${aws_instance.nat.id}"
+  vpc = true
+}
+
 /*===============
   Public Subnet
 ===============*/
